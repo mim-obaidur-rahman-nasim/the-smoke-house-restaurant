7b6af6cc56e25fdfe9be2bdfe813d30fdab31696
JAHID <jahidhasan6484>
2024-06-28 21:18:10 +0600

    register done with getting role in auth context

---
+++ src/pages/shared/Loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-500">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
